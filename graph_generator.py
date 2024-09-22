b2d4172eb685ca6fc4c25a946d7081c522e2051f
Mason Cong <masoncong1@gmail.com>
2024-09-22 00:53:26 -0400

    Update graph_generator.py

--- graph_generator.py
+++ graph_generator.py
@@ -1,17 +1,21 @@
 import networkx as net
+import matplotlib.pyplot as plt
 
 def watts_graphs():
     n = 1000
-    k = 3
-    p = 1/4
+    k = 4
+    p = 0.25
     #1
     G = net.watts_strogatz_graph(n, k, p)
     net.draw(G)
+    plt.show()
+    #list(G.nodes)S
     #2
     #p = 1/5
     #G = net.watts_strogatz_graph(n, k, p)
     #3
     #p = 1/6
     #G = net.watts_strogatz_graph(n, k, p)
+
 watts_graphs()
 
